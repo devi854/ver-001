4a46aa37a1a24dd37ceac049d91927e36666ccd6
prasad <prasadkesari329.dk@gmail.com>
2023-04-22 12:55:51 +0530

    newre

---
+++ demo-db/dbo/Tables/Products.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Products] (
+    [ProductId] INT   NOT NULL,
+    [Name]      TEXT  NULL,
+    [Price]     MONEY NULL,
+    [Description] TEXT NULL, 
+    PRIMARY KEY CLUSTERED ([ProductId] ASC)
+);
+
